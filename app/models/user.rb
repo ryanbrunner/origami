4d84676a72c2985603d9ccc8180c814ca6c2159b
Libin Pan <libinpan@gmail.com>
2011-04-28 21:05:51 -0400

    More devise view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  field :name
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  
 end
