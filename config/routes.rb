747bd1c231b9facdfab03a6368fc19b1ae520377
Aaron McLeod <sircoolguy99@gmail.com>
2011-05-12 20:58:27 -0400

    added basic user management

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Origami::Application.routes.draw do
   devise_for :users
 
   resources :posts, :except => :show
-  resources :users, :only => [:index, :show]
+  resources :users, :only => [:index, :show, :edit, :update]
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
