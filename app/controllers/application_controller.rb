fba85e9d44a323b9e9cd2bcd02d06651f4ce5ab9
Marius Butuc <marius.butuc@gmail.com>
2011-05-12 21:31:26 -0400

    Basic authorization with CanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  check_authorization :unless => :devise_controller?
   protect_from_forgery
 
   layout proc{ |c| c.request.xhr? ? false : "application" }
