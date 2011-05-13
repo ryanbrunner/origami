e0b810fe419d544c0e8bea81d53dd7d78b949c28
Aaron McLeod <sircoolguy99@gmail.com>
2011-05-12 20:06:57 -0400

    added cancan ability, and minor changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    render :text => 'Welcome to Origami!'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,24 @@
 class User
   include Mongoid::Document
+  
+  ROLES = %w[admin user]
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  attr_accessible :email, :password, :password_confirmation, :remember_me
   
   field :name
+  field :role
   validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
+  def admin?
+    self.role == ROLES[0]
+  end
+  
+  def user?
+    self.role == ROLES[1]
+  end
 end
